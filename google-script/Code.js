60c4d4e9abedae4d12f18e1dac2c18428a66e0a3
Jakub Bouček <pan@jakubboucek.cz>
2019-06-10 16:04:59 +0200

    Google script - change from web app to api app

--- google-script/Code.js
+++ google-script/Code.js
@@ -1,34 +1,22 @@
-function doGet(request) {
-    return HtmlService.createTemplateFromFile('Index')
-        .evaluate();
-}
-
-function include(filename) {
-    return HtmlService.createHtmlOutputFromFile(filename)
-        .getContent();
-}
-
-function myFunction() {
-    var contacts = ContactsApp.getContactsByUrl("google");
-
-    for (var i in contacts) {
-        var contact = contacts[i];
-        var info = Utilities.formatString("%s", contact.getFullName());
-
-        var urls = [];
-        contact.getUrls().forEach(function (url) {
-            var isGPlus = url.getAddress().match(/plus.google.com/) || url.getAddress().match(/google.com\/profiles\//);
-            if (isGPlus) {
-                var urlinfo = Utilities.formatString(" - [%s] %s", url.getLabel(), url.getAddress());
-                urls.push(urlinfo);
-            }
+function getAffectedContacts() {
+  var contacts = ContactsApp.getContactsByUrl("google");
+  
+  var rows = [];
+  
+  for(i in contacts) {
+    var contact = contacts[i];
+    
+    contact.getUrls().forEach(function(url){
+      var isGPlus = url.getAddress().match(/plus.google.com/) || url.getAddress().match(/google.com\/profiles\//);
+      if(isGPlus){
+        rows.push({
+          'id': contact.getId(),
+          'name': contact.getFullName(),
+          'url': url.getAddress()
         });
-
-        if (urls.length) {
-            Logger.log(info);
-            urls.forEach(function (url) {
-                Logger.log(url)
-            });
-        }
-    }
+      }
+    });
+  }
+  
+  return rows;
 }
