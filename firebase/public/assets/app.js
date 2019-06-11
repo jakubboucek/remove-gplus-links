fc888b9829307febd9ef97fffcaa3ac71f3e7267
Jakub Bouček <pan@jakubboucek.cz>
2019-06-11 14:18:21 +0200

    WIP progress

--- firebase/public/assets/app.js
+++ firebase/public/assets/app.js
@@ -1,3 +1,86 @@
-document.addEventListener('DOMContentLoaded', function() {
-    window.app = firebase.app();
+document.addEventListener('DOMContentLoaded', () => {
+    const permissions = [
+        'https://www.google.com/m8/feeds'
+    ];
+
+    const app = firebase.app();
+    let token;
+
+    const auth = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        permissions.forEach((perm) => provider.addScope(perm));
+        firebase.auth().signInWithPopup(provider).then((response) => {
+            token = response.credential.accessToken;
+            console.log(token);
+        });
+
+    };
+
+    const vue = new Vue({
+        el: '#app',
+        data: {},
+        methods: {
+            signIn: auth
+        }
+    });
+
+
+    const gs = new GoogleScript(
+        "MPvwfpzrIThutAMpQ8yrDkl_VCDNGuL86",
+        "**REDACTED**"
+    );
+    let contacts = new Contacts(gs);
+
+    console.log(contacts.getAffectedContacts());
+
 });
+
+class Contacts {
+    constructor(googleScript) {
+        if (googleScript instanceof GoogleScript === false) {
+            throw new Error("googleScript must be instance of GoogleScript");
+        }
+
+        this.gs = googleScript;
+    }
+
+    async getAffectedContacts() {
+        return await this.gs.run('getAffectedContacts');
+    }
+}
+
+class GoogleScript {
+    constructor(scriptId, accessToken) {
+        this.endpoint = "https://script.googleapis.com/v1/scripts/" + scriptId + ":run";
+        this.accessToken = accessToken;
+    }
+
+    async run(method, parameters) {
+        let bodyObj = {
+            "function": method,
+            "parameters": Array.isArray(parameters) ? parameters : [],
+        };
+
+        let response = await fetch(this.endpoint, {
+            body: JSON.stringify(bodyObj),
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'omit',
+            headers: {
+                'Authorization': 'Bearer ' + this.accessToken,
+                'Content-Type': 'application/json'
+            },
+            redirect: 'error',
+            referrer: 'client'
+        });
+
+        let body = await response.json();
+
+        if (response.ok && body.done === true && body.hasOwnProperty('response')) {
+            return body.response.result;
+        } else {
+            throw body;
+        }
+    }
+}
--- google-script/Code.js
+++ google-script/Code.js
@@ -1,3 +1,21 @@
+/**
+ * Test connectivity endpoint only - just return value of first argument if defined, or `true`
+ * @param testArg argument to reflect their value to response
+ * @returns True or value of `testArg` argument
+ */
+function checkConnectivity(testArg) {
+  if(arguments[0] !== undefined) {
+    return arguments[0];
+  }
+  
+  return true;
+}
+
+/**
+ * Return G+ profile links in contacts
+ * If one contact contain more than one profile link, it be returned repeately for each link
+ * @returns {Object[]} Array od contacts
+ */
 function getAffectedContacts() {
   var contacts = ContactsApp.getContactsByUrl("google");
   
